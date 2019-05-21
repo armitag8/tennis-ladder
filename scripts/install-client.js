91a26cd15ba53f1a49f8b24a650ed4589c10cc77
Joe Armitage <joe.armitage@mail.utoronto.ca>
2019-05-21 18:29:48 -0400

    Less hacky solution to no config file on CI test server.

--- scripts/install-client.js
+++ scripts/install-client.js
@@ -1,8 +1,13 @@
-require("fs").copyFile("config.json", "client/src/config.json", err => {
-    if (err) throw err;
-    require("child_process").spawn(
-        "npm", 
-        [ 'install' ],
-         { stdio: 'inherit', cwd: 'client', shell: true 
+const installClient = () => require("child_process").spawn(
+    "npm",
+    ['install'],
+    {
+        stdio: 'inherit', cwd: 'client', shell: true
     });
-});
+
+if (process.env.NODE_ENV !== "test")
+    require("fs").copyFile("config.json", "client/src/", err => {
+        if (err) throw err;
+        installClient();
+    });
+else installClient();
