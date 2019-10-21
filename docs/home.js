bd87f4394610269d2b05265e7d09d58b25651632
armitag8 <joe.armitage@mail.utoronto.ca>
2019-10-20 22:23:36 -0400

    Create home.js

---
+++ docs/home.js
@@ -0,0 +1,26 @@
+const home = `
+<h3>
+  The Ladder is done for now
+</h3>
+
+<p> 
+  Thank-you for your interest in the ladder.
+</p>
+
+<hr/>
+
+<p>
+  Unfortunately, the season is over and no more matches will be scheduled, played or recorded.
+</p>
+
+<h4>Next Season</h4>
+<p>
+If you are interested in signing up for next season, or would like to know more:
+<ul>
+        <li>Email <a href=javascript:window.location.href=atob("bWFpbHRvOnRlbm5pcy51dHNjQGdtYWlsLmNvbQ==")>the ladder admin</a></li>
+        <li>If you're techy and bored, check out the <a href="https://github.com/armitag8/tennis-ladder">code</a></li>
+    </ul>
+</p>
+`;
+
+export {home};
